201624b16c7a102a39763a923832421f731727d3
Sherlock Holmes <sherlockholmesxp@gmail.com>
2026-02-25 21:32:46 +0800

    fix: add bdg scripting

--- src/template.py
+++ src/template.py
@@ -8,12 +8,18 @@ context.binary = {bin_name}
 context.terminal = "kitty"
 context.gdb_binary = "/usr/bin/pwndbg"
 
+gdbscript = """
+set breakpoint pending on
+b *main
+continue
+"""
+
 
 def conn():
     if args.LOCAL:
         r = process({proc_args})
         if args.GDB:
-            gdb.attach(r)
+            gdb.attach(r, gdbscript=gdbscript)
     else:
         r = remote("addr", 1337)
 
